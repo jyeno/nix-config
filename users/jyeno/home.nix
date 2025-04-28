13d72e1971b47b9a2527a6a3b3cb0be48ba72655
Jean Lima Andrade <jeno.andrade@gmail.com>
2025-04-28 10:55:29 -0300

    ashell: Update module settings

--- users/jyeno/home.nix
+++ users/jyeno/home.nix
@@ -165,7 +165,91 @@
       };
       hyprlock.enable = true;
       river.enable = false;
-      ashell.enable = true;
+      ashell = {
+        enable = true;
+        config = let
+          terminal = pkgs.lib.getExe pkgs.ghostty;
+          pavucontrol = pkgs.lib.getExe pkgs.pavucontrol;
+          lockCmd = "${pkgs.lib.getExe pkgs.hyprlock} &";
+          clockFormat = "%a %d %b %R";
+          launcher = pkgs.lib.getExe pkgs.wofi;
+          cliphist = pkgs.lib.getExe pkgs.cliphist;
+          clipboard = "selected=$(${cliphist} list | ${launcher} -S dmenu) && echo \"$selected\" | ${cliphist} decode | wl-copy";
+          textCap = "150";
+          fontName = "Comic Sans MS"; # TODO change
+          backgroundColor = "#1e1e2e";
+          primaryColor = "#fab387";
+          secondaryColor = "#11111b";
+          successColor = "#a6e3a1";
+          dangerColor = "#f38ba8";
+          textColor = "#f38ba8";
+        in {
+          logLevel = "WARN";
+          outputs = "All";
+          position = "Bottom";
+          appLauncherCmd = launcher;
+          clipboardCmd = clipboard;
+          truncateTitleAfterLength = textCap;
+          modules = {
+            left = [
+              "AppLauncher"
+              "Workspaces"
+              "WindowTitle"
+            ];
+            center = ["MediaPlayer"];
+            right = [
+              "Tray"
+              "SystemInfo"
+              ["Clock" "Clipboard" "Privacy" "Settings"]
+            ];
+          };
+          workspaces = {
+            visibilityMode = "All";
+            enableWorkspaceFilling = false;
+          };
+          system = {
+            cpuWarnThreshold = 60;
+            cpuAlertThreshold = 80;
+            memWarnThreshold = 70;
+            memAlertThreshold = 85;
+            tempWarnThreshold = 60;
+            tempAlertThreshold = 80;
+          };
+
+          clock.format = clockFormat;
+
+          mediaPlayer.maxTitleLength = textCap;
+
+          settings = {
+            lockCmd = lockCmd;
+            audioSinksMoreCmd = "${pavucontrol} -t 3";
+            audioSourcesMoreCmd = "${pavucontrol} -t 4";
+            wifiMoreCmd = "${terminal} --command=iwctl";
+            vpnMoreCmd = "${terminal} --command=iwctl";
+            bluetoothMoreCmd = "${terminal} --command=bluetoothctl";
+          };
+
+          appearance = {
+            fontName = fontName;
+            style = "islands";
+            opacity = 1.0;
+            backgroundColor = backgroundColor;
+            primaryColor = primaryColor;
+            secondaryColor = secondaryColor;
+            successColor = successColor;
+            dangerColor = dangerColor;
+            textColor = textColor;
+            workspaceColors = [
+              "#fab387"
+              "#b4befe"
+            ];
+            specialWorkspaceColors = [
+              "#a6e3a1"
+              "#f38ba8"
+            ];
+          };
+        };
+      };
       gbar.enable = false;
       waybar.enable = false;
       wofi.enable = true;
