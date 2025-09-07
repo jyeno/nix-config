8e290af12fc055634d6f80b8f8a8797adf1c92ae
Jean Lima Andrade <jeno.andrade@gmail.com>
2025-09-07 12:24:49 -0300

    droid: WIP initial modules/ support

--- extras/droid/configuration.nix
+++ extras/droid/configuration.nix
@@ -3,6 +3,7 @@
   pkgs,
   ...
 }: {
+  # documentation.man.enable = false;
   environment.packages = with pkgs; [
     neovim
     git
@@ -10,10 +11,80 @@
   ];
   environment.etcBackupExtension = ".bak";
 
+  user = {
+    shell = pkgs.fish;
+    # userName = "jyeno";
+  };
+
+  terminal = {
+    colors = {
+      background = "181818";
+      foreground = "FFFFFF";
+      cursor = "585858";
+      color0 = "22273d";
+      color1 = "374059";
+      color2 = "525866";
+      color3 = "878d96";
+      color4 = "c8c8c8";
+      color5 = "ffffff";
+      color6 = "ffffff";
+      color7 = "ffffff";
+      color8 = "fa7883";
+      color9 = "ffc387";
+      color10 = "ff9470";
+      color11 = "98c379";
+      color12 = "8af5ff";
+      color13 = "6bb8ff";
+      color14 = "e799ff";
+      color15 = "b3684f";
+    };
+    # font = pkgs.nerd-fonts.iosevka;
+  };
+
   nix.extraOptions = ''
     experimental-features = nix-command flakes pipe-operators
   '';
   time.timeZone = "America/Bahia";
+
+  home-manager.config = {
+    programs.home-manager.enable = true;
+    systemd.user.startServices = "sd-switch";
+    home = {
+      packages = with pkgs; [
+        xz
+        ripgrep
+        jq
+        eza
+        dnsutils
+        socat
+        nmap
+        file
+        which
+        gnutar
+        strace
+        nix-output-monitor
+      ];
+
+      stateVersion = "24.05";
+    };
+
+    local.home.cli = {
+      fish = {
+        enable = true;
+        starship.enable = true;
+      };
+      git.enable = true;
+      gpg.enable = true;
+      nvf.enable = true;
+      fd.enable = true;
+      fzf.enable = true;
+      aria2.enable = true;
+      bat.enable = true;
+      ssh.enable = true;
+      tmux.enable = true;
+      direnv.enable = true;
+    };
+  };
 
   system.stateVersion = "24.05";
 }
--- extras/droid/flake.nix
+++ extras/droid/flake.nix
@@ -1,20 +1,58 @@
 {
   description = "basic Nix-on-Droid flake system configuration";
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvf.url = "github:notashelf/nvf?ref=v0.8";
+
+    plasma-manager.url = "github:nix-community/plasma-manager";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixdroid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    nixdroid,
-  }: {
-    nixOnDroidConfigurations.default = nixdroid.lib.nixOnDroidConfiguration {
-      pkgs = import nixpkgs {system = "aarch64-linux";};
-      modules = [./configuration.nix];
+  outputs = inputs: let
+    inherit (builtins) attrValues mapAttrs;
+    inherit (inputs.nixpkgs) lib legacyPackages;
+    localLib = import ../../lib {inherit inputs;};
+    discoveredHomeModules = localLib.discoverModules ../../modules/hm;
+
+    specialArgs = {
+      homeSpecificModules = [
+        inputs.sops-nix.homeManagerModules.sops
+        inputs.nvf.homeManagerModules.default
+        inputs.impermanence.nixosModules.home-manager.impermanence
+        inputs.plasma-manager.homeManagerModules.plasma-manager
+      ];
+    };
+  in {
+    nixOnDroidConfigurations.default = inputs.nixdroid.lib.nixOnDroidConfiguration {
+      pkgs = import inputs.nixpkgs {
+        system = "aarch64-linux";
+        config.allowUnfree = true;
+      };
+      modules = [
+        # inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            backupFileExtension = "backup";
+            sharedModules = (specialArgs.homeSpecificModules or []) ++ (attrValues discoveredHomeModules);
+          };
+        }
+        ./configuration.nix
+      ];
     };
   };
 }
