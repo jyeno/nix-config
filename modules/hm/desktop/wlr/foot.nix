5f0f96b832885f1ec169f4d2ea306652422c3965
Jean Lima Andrade <jeno.andrade@gmail.com>
2025-10-10 01:05:20 -0300

    hm/foot: update settings

--- modules/hm/desktop/wlr/foot.nix
+++ modules/hm/desktop/wlr/foot.nix
@@ -13,36 +13,21 @@ in {
   config = lib.mkIf cfg.enable {
     # fix to run footclient
     #TODO add more options
-    systemd.user.services.foot.Service.Environment = ["PATH=/run/current-system/sw/bin/"];
+    # systemd.user.services.foot.Service.Environment = ["PATH=/run/current-system/sw/bin/"];
 
     programs.foot = {
       enable = true;
       server.enable = true;
 
-      settings = let
-        fish = lib.getExe pkgs.fish;
-        notify-send = lib.getExe' pkgs.libnotify "notify-send";
-        xdg-open = lib.getExe' pkgs.xdg-utils "xdg-open";
-      in {
+      settings = {
         main = {
-          term = "foot";
-          shell = "${fish}";
-          # pad = "15x15";
-          notify = "${notify-send} -a \${app-id} -i \${app-id} \${title} \${body}";
+          term = "xterm-256color";
           selection-target = "clipboard";
         };
 
         scrollback = {
           lines = 100000;
           multiplier = 3;
-        };
-
-        url = {
-          launch = "${xdg-open} \${url}";
-          label-letters = "sadfjklewcmpgh";
-          osc8-underline = "url-mode";
-          protocols = "http, https, ftp, ftps, file";
-          uri-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,~:;/?#@!$&%*+=\"'()[]";
         };
 
         mouse.hide-when-typing = "yes";
