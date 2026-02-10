6b9c0e45cb9d1b6e51c2206a64e7f73c6851234a
Jean Lima Andrade <jeno.andrade@gmail.com>
2026-02-10 12:30:02 -0300

    WIP: move agenix jyeno key to sys instead of hm

--- modules/users/jyeno/secrets.nix
+++ modules/users/jyeno/secrets.nix
@@ -8,21 +8,21 @@
       inputs.self.modules.nixos.secrets
     ];
     users.users.jyeno.hashedPasswordFile = config.age.secrets.jyeno-pass.path;
-    age.secrets."jyeno-pass".file = "${inputs.secrets}/users/jyeno.age";
+    age = {
+      secrets."jyeno-pass".file = "${inputs.secrets}/users/jyeno.age";
+      secrets."jyeno-key" = {
+        file = "${inputs.secrets}/users/jyeno-key.age";
+        path = "${config.home-manager.users.jyeno.home.homeDirectory}/.ssh/id_ed25519";
+        mode = "0700";
+        owner = "jyeno";
+        group = "jyeno";
+      };
+    };
   };
 
-  flake.modules.homeManager.jyeno = {
-    self,
-    config,
-    ...
-  }: {
+  flake.modules.homeManager.jyeno = {config, ...}: {
     imports = [
       inputs.self.modules.homeManager.secrets
-    ];
-    age.secrets."jyeno-key" = {
-      file = "${inputs.secrets}/users/jyeno-key.age";
-      path = "${config.home.homeDirectory}/.ssh/id_ed25519";
-      mode = "0700";
-    };
+    ]; # the best would be to add jyeno-key here but cant because the default key requires password
   };
 }
